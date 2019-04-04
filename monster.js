7524485d33f6d74a05855b44280b72d5b26ea7d6
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-04-04 10:22:03 -0400

    initial commit

--- monster.js
+++ monster.js
@@ -1,3 +1,23 @@
 const monster = {
-
+    isAlive: true,
+    hitPoints: 30,
+    lastDamageTaken: 0,
+    respawn: function() {
+        this.Alive = true;
+        this.hitPoints = 30;
+        this.lastDamageTaken = 0; 
+    },
+    calculateRawDamage: function() {
+        return Math.floor(Math.random() * 6) + 2;
+        
+    },
+    takeDamage: function(enemy) {
+        const currentDamage = enemy
+        this.hitPoints = this.hitPoints - currentDamage;
+        this.lastDamageTaken = currentDamage;
+        if(this.hitPoints < 0){
+            this.isAlive = false;
+        }
+    },
+    
 };
--- player.js
+++ player.js
@@ -1,3 +1,27 @@
 const player = {
+    isAlive: true,
+    hitPoints: 100,
+    level: 1,
+    lastDamageTaken: 0,
+    baseAttackDamage: 5,
+    levelUp: function(){
+        this.level = this.level + 1;
+        this.hitPoints = this.hitPoints + 5;
+        this.lastDamageTaken = 0;
+        this.baseAttackDamage = this.baseAttackDamage * (20/100); 
+    },
+    fight: function(enemy) {
+        const currentDamage = enemy.calculateRawDamage();
+        this.hitPoints = this.hitPoints - currentDamage;
+        this.lastDamageTaken = currentDamage
+        if(this.hitPoints < 0){
+            this.isAlive = false;
+        }
+        enemy.takeDamage(this.calculateRawDamage())
+    },
+    calculateRawDamage: function() {
+        return Math.floor(Math.random() * 6);
+
+    }
 
 }
